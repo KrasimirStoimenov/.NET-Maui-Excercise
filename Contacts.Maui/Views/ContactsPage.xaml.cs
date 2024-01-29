5b4b2e4e5962ae9b4d73477c9325a7f2d2390d2d
Krassimir Stoimenov <krassimir.stoimenov@gmail.com>
2024-01-29 21:35:31 +0200

    Added simple contacts class and display them into page.

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -5,6 +5,15 @@ public partial class ContactsPage : ContentPage
     public ContactsPage()
     {
         InitializeComponent();
+
+        List<Contact> contacts = new()
+        {
+            new Contact("First Contact", "First Email"),
+            new Contact("Second Contact", "Second Email"),
+            new Contact("Third Contact", "Third Email"),
+        };
+
+        listContacts.ItemsSource = contacts;
     }
 
     private void btnAddContact_Clicked(object sender, EventArgs e)
@@ -15,5 +24,18 @@ public partial class ContactsPage : ContentPage
     private void btnEditContact_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync(nameof(EditContactPage));
+    }
+
+    public class Contact
+    {
+        public Contact(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+        }
+
+        public string Name { get; init; }
+
+        public string Email { get; init; }
     }
 }
